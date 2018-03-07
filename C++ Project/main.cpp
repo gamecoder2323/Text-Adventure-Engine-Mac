40b0ce9c0a107aa8a3c653a8f702c5e7befce4b2
Reuben Randell <reubenrandell@Reubens-MacBook.local>
2018-03-06 20:23:28 -0600

    Initial Commit

---
+++ C++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Project
+//
+//  Created by Reuben Randell on 3/6/18.
+//  Copyright © 2018 Stokesoft. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
